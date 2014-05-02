3e1af46e2768477d1d6b15c5023999af84a0bcdb
Somkiat <somkiat.p@gmail.com>
2014-05-02 18:56:15 +0700

    Create application

--- src/main/java/up1/demo/DemoApplication.java
+++ src/main/java/up1/demo/DemoApplication.java
@@ -1,9 +1,30 @@
 package up1.demo;
 
-public class DemoApplication {
-	
-	public static void main(String[] args) {
-		
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class DemoApplication extends Application<DemoConfiguration> {
+
+	public static void main(String[] args) throws Exception {
+		new DemoApplication().run(args);
+	}
+
+	@Override
+	public String getName() {
+		return "Demo";
+	}
+
+	@Override
+	public void initialize(Bootstrap<DemoConfiguration> arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run(DemoConfiguration arg0, Environment arg1) throws Exception {
+		// TODO Auto-generated method stub
+
 	}
 
 }
